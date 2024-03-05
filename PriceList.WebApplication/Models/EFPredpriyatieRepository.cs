90b40a909f919f8ec1b29f09b47a7b11b4a6a9cd
TihonchukArtyom89 <mortus1989@gmail.com>
2024-03-05 18:11:25 +0300

    add two implementations of repositories interfaces (EFStoreRepository cs and EFPredpriyatieRepository cs) in Models folder

---
+++ PriceList.WebApplication/Models/EFPredpriyatieRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace PriceList.WebApplication.Models;
+
+public class EFPredpriyatieRepository
+{
+    private PredpriyatieDbContext context;
+    public EFPredpriyatieRepository(PredpriyatieDbContext ctx)
+    {
+        context = ctx;
+    }
+
+    public IQueryable<PriceList> PriceLists => context.PriceLists;
+}
---
+++ PriceList.WebApplication/Models/EFStoreRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace PriceList.WebApplication.Models;
+
+public class EFStoreRepository: IStoreRepository
+{
+    private PredpriyatieDbContext context;
+    public EFStoreRepository(PredpriyatieDbContext ctx)
+    {
+        context = ctx;
+    }
+    public IQueryable<Product> Products => context.Products;
+}
