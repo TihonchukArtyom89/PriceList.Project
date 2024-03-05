9fc402c56e2a4b527c2071d428d9c2e1be518987
TihonchukArtyom89 <mortus1989@gmail.com>
2024-03-05 14:04:46 +0300

    add HomeController cs to Controllers folder, add Index cshtml to Views folder

---
+++ PriceList.WebApplication/Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PriceList.WebApplication.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
