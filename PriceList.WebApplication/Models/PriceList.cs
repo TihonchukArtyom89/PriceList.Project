deff2066691c0de209cb4181fb0b264766c85f41
TihonchukArtyom89 <mortus1989@gmail.com>
2024-03-05 16:52:39 +0300

    add Product cs and PriceList cs to Models folder

---
+++ PriceList.WebApplication/Models/PriceList.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PriceList.WebApplication.Models;
+
+public class PriceList
+{
+    public long? PriceListID { get; set; }
+    public string PriceListName { get; set; } = String.Empty;
+    public DateTime PriceListDateCreation { get; set; }
+    public DateTime PriceListDateModyfycation { get; set; }
+    public long[] PriceListProductIDs { get; set; } = Array.Empty<long>();//массив ид продуктов в этом прайс листе
+    /*добавление новых столбцов с разными типами данных реализовать через метод массив? список? коллекция?*/
+}
---
+++ PriceList.WebApplication/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PriceList.WebApplication.Models;
+public class Product
+{    public long? ProductID { get; set; }
+    public string ProductName { get; set; } = string.Empty;
+    public string ProductDescription { get; set; } = string.Empty;
+    public string ProductCategory { get; set; } = string.Empty;
+
+    [Column(TypeName ="decimal(8,2)")]
+    public decimal ProductPrice { get; set; }
+
+}
