5742798126d1e18dcfb2b411aaab899d61af3599
TihonchukArtyom89 <mortus1989@gmail.com>
2024-03-05 18:43:04 +0300

    add initial migration

--- PriceList.WebApplication/Models/PriceList.cs
+++ PriceList.WebApplication/Models/PriceList.cs
@@ -9,6 +9,6 @@ public class PriceList
     public string PriceListName { get; set; } = String.Empty;
     public DateTime PriceListDateCreation { get; set; }
     public DateTime PriceListDateModyfycation { get; set; }
-    public long[] PriceListProductIDs { get; set; } = Array.Empty<long>();//массив ид продуктов в этом прайс листе
+    //public long[] PriceListProductIDs { get; set; } = Array.Empty<long>();//массив ид продуктов в этом прайс листе
     /*добавление новых столбцов с разными типами данных реализовать через метод массив? список? коллекция?*/
 }
