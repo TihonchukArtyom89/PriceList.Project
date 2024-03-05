0110907611b48f3ae179325abb6a3b6dd4bd230d
TihonchukArtyom89 <mortus1989@gmail.com>
2024-03-05 17:19:48 +0300

    install ef core package,add connection string in appsettings json,add db context class (PredpriyatieDbContext cs) in Models folder,configure ef core in Program cs by modifying request pipeline

---
+++ PriceList.WebApplication/Models/PredpriyatieDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PriceList.WebApplication.Models;
+
+public class PredpriyatieDbContext : DbContext
+{
+    public PredpriyatieDbContext(DbContextOptions<PredpriyatieDbContext> options) : base(options) { }
+    public DbSet<Product> Products => Set<Product>();
+    public DbSet<PriceList> PriceLists => Set<PriceList>();
+}
--- PriceList.WebApplication/Program.cs
+++ PriceList.WebApplication/Program.cs
@@ -1,5 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+using PriceList.WebApplication.Models;  
+
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddControllersWithViews();//вызов объектов необходимых для mvc и 
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<PredpriyatieDbContext>(opts => { opts.UseSqlServer(builder.Configuration["ConnectionStrings:PriceList.WebApplicationConnection"]); });
 var app = builder.Build();
 app.UseStaticFiles();
 app.MapDefaultControllerRoute();
