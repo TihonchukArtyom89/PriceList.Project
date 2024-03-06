2170b7d5e0a0e04758ccc7ed37972cdae7f81349
TihonchukArtyom89 <mortus1989@gmail.com>
2024-03-06 19:14:54 +0300

    add new unit test (Can_Send_Pagination_View_Model) to check sending pagination view model to homecontrroller in HomeControllerTests cs from PriceList.Test project,update all tests in this class

--- PriceList.Tests/HomeControllerTests.cs
+++ PriceList.Tests/HomeControllerTests.cs
@@ -1,3 +1,5 @@
+using PriceList.WebApplication.Models.ViewModels;
+
 namespace PriceList.Tests;
 
 public class HomeControllerTests
@@ -14,9 +16,9 @@ public class HomeControllerTests
         }).AsQueryable());
         HomeController homeController = new(mockStoreRepository.Object);
         //Act
-        IEnumerable<Product>? resultProduct = (homeController.Index() as ViewResult)?.ViewData.Model as IEnumerable<Product>;
+        ProductsListViewModels resultProduct = homeController.Index()?.ViewData.Model as ProductsListViewModels ?? new();
         //Assert
-        Product[] productsArray = resultProduct?.ToArray() ?? Array.Empty<Product>();
+        Product[] productsArray = resultProduct.Products.ToArray();
         Assert.True(productsArray.Length == 2);
         Assert.Equal("P1", productsArray[0].ProductName);
         Assert.Equal("P2", productsArray[1].ProductName);
@@ -34,14 +36,37 @@ public class HomeControllerTests
             new() { ProductID = 4, ProductName = "P4" },
             new() { ProductID = 5, ProductName = "P5" }
         }).AsQueryable());
-        HomeController homeController = new(mockStoreRepository.Object);
-        homeController.PageSize = 3;
+        HomeController homeController = new(mockStoreRepository.Object) { PageSize = 3 };
+        //homeController.PageSize = 3;
         //Act
-        IEnumerable<Product>? resultProduct = (homeController.Index(2) as ViewResult)?.ViewData.Model as IEnumerable<Product> ?? Enumerable.Empty<Product>();
+        ProductsListViewModels resultProduct = homeController.Index(2)?.ViewData.Model as ProductsListViewModels ?? new();
         //Assert
-        Product[] productsArray = resultProduct.ToArray();
+        Product[] productsArray = resultProduct.Products.ToArray();
         Assert.True(productsArray.Length == 2);
         Assert.Equal("P4", productsArray[0].ProductName);
         Assert.Equal("P5", productsArray[1].ProductName);
+    }
+    [Fact]
+    public void Can_Send_Pagination_View_Model()
+    {
+        //Arrange
+        Mock<IStoreRepository> mockStoreRepository = new();
+        mockStoreRepository.Setup(m => m.Products).Returns((new Product[]
+        {
+            new() { ProductID = 1, ProductName = "P1" },
+            new() { ProductID = 2, ProductName = "P2" },
+            new() { ProductID = 3, ProductName = "P3" },
+            new() { ProductID = 4, ProductName = "P4" },
+            new() { ProductID = 5, ProductName = "P5" }
+        }).AsQueryable());
+        HomeController homeController = new(mockStoreRepository.Object) { PageSize = 3 };
+        //Act
+        ProductsListViewModels resultProduct = homeController.Index(2)?.ViewData.Model as ProductsListViewModels ?? new();
+        //Assert
+        PagingInfo pageIngo = resultProduct.PagingInfo;
+        Assert.Equal(2,pageIngo.CurrentPage);
+        Assert.Equal(3, pageIngo.ItemsPerPage);
+        Assert.Equal(5, pageIngo.TotalItems);
+        Assert.Equal(2, pageIngo.TotalPages);
     }
 }
--- PriceList.WebApplication/Controllers/HomeController.cs
+++ PriceList.WebApplication/Controllers/HomeController.cs
@@ -1,20 +1,31 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using PriceList.WebApplication.Models;
+using PriceList.WebApplication.Models.ViewModels;
 
 namespace PriceList.WebApplication.Controllers;
 
 public class HomeController : Controller
 {
-    private IStoreRepository _storeRepository;
+    private IStoreRepository storeRepository;
     public int PageSize = 4;
 
-    public HomeController(IStoreRepository storeRepository)
+    public HomeController(IStoreRepository _storeRepository)
     {
-        _storeRepository = storeRepository;
+        storeRepository = _storeRepository;
     }
 
     public ViewResult Index(int productPage=1)
     {
-        return View(_storeRepository.Products.OrderBy(p=>p.ProductID).Skip((productPage-1)*PageSize).Take(PageSize));
+        //return View(_storeRepository.Products.OrderBy(p=>p.ProductID).Skip((productPage-1)*PageSize).Take(PageSize));
+        return View(new ProductsListViewModels
+        {
+            Products = storeRepository.Products.OrderBy(p => p.ProductID).Skip((productPage - 1) * PageSize).Take(PageSize),
+            PagingInfo = new PagingInfo 
+            {
+                CurrentPage = productPage,
+                ItemsPerPage = PageSize,
+                TotalItems = storeRepository.Products.Count()  
+            }
+        }); 
     }
 }
