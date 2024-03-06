2bed61600fb61665183c07d9e4f8a99fd6d2c1b3
TihonchukArtyom89 <mortus1989@gmail.com>
2024-03-06 18:52:55 +0300

    add view data model fo products list (ProductsListViewModels cs) in Models/ViewModels folder

---
+++ PriceList.WebApplication/Models/ViewModels/ProductsListViewModels.cs
@@ -0,0 +1,8 @@
+﻿
+namespace PriceList.WebApplication.Models.ViewModels;
+
+public class ProductsListViewModels
+{
+    public IEnumerable<Product> Products { get; set; } = Enumerable.Empty<Product>();
+    public PagingInfo PagingInfo { get; set; } = new();
+}
