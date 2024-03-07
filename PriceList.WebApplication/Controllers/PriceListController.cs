9d969cb0ab4d918b6b13ff47feead9ed5b62e1a0
TihonchukArtyom89 <mortus1989@gmail.com>
2024-03-07 12:11:24 +0300

    add controller and view to work with price lists (PriceListsController cs in Controllers Folder and ListOfPriceLists cshtml in Views folder), add paging infrastructure for this view (ListOfPriceLists cs in Models/ViewModels folder),work on layout view (_Layout cshtml in Views/Shared folder),create todo list in TODO txt file

--- PriceList.WebApplication/Controllers/PriceListController.cs
+++ PriceList.WebApplication/Controllers/PriceListController.cs
@@ -1,18 +1,29 @@
-﻿//using Microsoft.AspNetCore.Mvc;
-//using PriceList.WebApplication.Models;
+﻿using Microsoft.AspNetCore.Mvc;
+using PriceList.WebApplication.Models;
+using PriceList.WebApplication.Models.ViewModels;
 
-//namespace PriceList.WebApplication.Controllers;
+namespace PriceList.WebApplication.Controllers;
 
-//public class PriceListController
-//{
-//    private IPredpriyatieRepository _predpriyatieRepository;
-//    public PriceListController(IPredpriyatieRepository predpriyatieRepository)
-//    {
-//        _predpriyatieRepository = predpriyatieRepository;
-//    }
+public class PriceListController:Controller
+{
+    private IPredpriyatieRepository predpriyatieRepository;
+    public int PageSize = 4;
+    public PriceListController(IPredpriyatieRepository _predpriyatieRepository)
+    {
+        predpriyatieRepository = _predpriyatieRepository;
+    }
 
-//    public IActionResult ListOfPriceList()
-//    {
-//        return View(_predpriyatieRepository.PriceLists);
-//    }
-//}
+    public IActionResult ListOfPriceLists(int productPage = 1)
+    {
+        return View(new ListOfPriceListsViewModels
+        {
+            PriceLists = predpriyatieRepository.PriceLists.OrderBy(p => p.PriceListID).Skip((productPage - 1) * PageSize).Take(PageSize),
+            PagingInfo = new PagingInfo
+            {
+                CurrentPage = productPage,
+                ItemsPerPage = PageSize,
+                TotalItems = predpriyatieRepository.PriceLists.Count()
+            }
+        });
+    }
+}
---
+++ PriceList.WebApplication/Models/ViewModels/ListOfPriceListsViewModels.cs
@@ -0,0 +1,8 @@
+﻿
+namespace PriceList.WebApplication.Models.ViewModels;
+
+public class ListOfPriceListsViewModels
+{
+    public IEnumerable<PriceList> PriceLists { get; set; } = Enumerable.Empty<PriceList>();
+    public PagingInfo PagingInfo { get; set; } = new();
+}
