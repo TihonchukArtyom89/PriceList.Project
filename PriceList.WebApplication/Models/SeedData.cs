034f0e112c823f87b36fa6c55d60cf2e39eb971c
TihonchukArtyom89 <mortus1989@gmail.com>
2024-03-12 16:00:44 +0300

    add check on emptyness Categories table in database, update todo, work on exception

--- PriceList.WebApplication/Models/SeedData.cs
+++ PriceList.WebApplication/Models/SeedData.cs
@@ -21,30 +21,25 @@ public static class SeedData
         }
         if (!context.Products.Any())
         {
-            //code for insert sample data to table Categories(categories of product)
-            ////Create data to insert in table
-            Category c1 = new() { CategoryID = 1, CategoryName = "Мебель" };
-            Category c2 = new() { CategoryID = 2, CategoryName = "Фрукты" };
-            int num = context.Database.ExecuteSqlRaw("insert into Categories (CategoryName) values ('Мебель'), ('Фрукты')"); 
-            ////Set IDENTITY INSERT ON - to insert data in Categories table with custom id parameter
-            //int num = context.Database.ExecuteSqlRaw("SET IDENTITY_INSERT dbo.Categories ON");
-            //context.SaveChanges();
-            //WriteLine("\n\n\n\n\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n\n\n\n"+num+"\n\n\n\n\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n\n\n\n");
-            ////Insert data in Categories table
-            //context.Categories.AddRange(c1, c2);
-            //context.SaveChanges();
-            ////Set IDENTITY INSERT OFF - to autoincrement id parameter in table Categories
-            //num = context.Database.ExecuteSqlRaw("SET IDENTITY_INSERT dbo.Categories OFF");
-            WriteLine("\n\n\n\n\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n\n\n\n" + num + "\n\n\n\n\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n\n\n\n");
-            context.SaveChanges();
-            //fill Products Table            
+            string[] CategoryName = { "Мебель", "Фрукты" };
+            if (!context.Categories.Any())
+            {
+                //code for insert sample data to table Categories(categories of product)//корректно передать кириллицу надо
+                int num = context.Database.ExecuteSqlRaw("insert into Categories (CategoryName) values ('" + CategoryName[0] + "'), ('" + CategoryName[1] + "')");
+                WriteLine("\n\n\n\n\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n\n\n\n" + num + "\n\n\n\n\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n\n\n\n");
+                context.SaveChanges();
+            }
+            //get id from table Categoies
+            var CategoryID_Мебель = context.Categories.Where(c => c.CategoryName == "Мебель").Select(c => c.CategoryID).ToQueryString();
+            WriteLine("\n\n\n\n\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n\n\n\n" + CategoryID_Мебель + "\n\n\n\n\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n\n\n\n");
+            //fill Products Table
             context.Products.AddRange(
-                new Product { ProductName = "Стул", ProductDescription = "Обычный стул", ProductPrice = 1547.04m, CategoryID = c1.CategoryID },
-                new Product { ProductName = "Яблоко", ProductDescription = "Красное,наливное", ProductPrice = 196.67m, CategoryID = c2.CategoryID },
-                new Product { ProductName = "Слива", ProductDescription = "Спелая,садовая", ProductPrice = 378.00m, CategoryID = c2.CategoryID },
-                new Product { ProductName = "Стол", ProductDescription = "Для обеда в саду", ProductPrice = 3098.39m, CategoryID = c1.CategoryID },
-                new Product { ProductName = "Груша", ProductDescription = "Можно скушать", ProductPrice = 247.07m, CategoryID = c2.CategoryID },
-                new Product { ProductName = "Стол", ProductDescription = "Компьтерный стол", ProductPrice = 15999.98m, CategoryID = c1.CategoryID }
+                new Product { ProductName = "Стул", ProductDescription = "Обычный стул", ProductPrice = 1547.04m, CategoryID = 1 },
+                new Product { ProductName = "Яблоко", ProductDescription = "Красное,наливное", ProductPrice = 196.67m, CategoryID = 2 },
+                new Product { ProductName = "Слива", ProductDescription = "Спелая,садовая", ProductPrice = 378.00m, CategoryID = 2 },
+                new Product { ProductName = "Стол", ProductDescription = "Для обеда в саду", ProductPrice = 3098.39m, CategoryID = 1 },
+                new Product { ProductName = "Груша", ProductDescription = "Можно скушать", ProductPrice = 247.07m, CategoryID = 2 },
+                new Product { ProductName = "Стол", ProductDescription = "Компьтерный стол", ProductPrice = 15999.98m, CategoryID = 1 }
                 );
             context.SaveChanges();
         }
