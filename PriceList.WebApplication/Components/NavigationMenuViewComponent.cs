f708da54056b3f718a49066e09ce10060b213e05
TihonchukArtyom89 <mortus1989@gmail.com>
2024-04-01 19:19:01 +0300

    add navigation  view component (NaveigationMenuViewComponent cs in Components folder), change _Layout to use navigation view component (not properly working)

---
+++ PriceList.WebApplication/Components/NavigationMenuViewComponent.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PriceList.WebApplication.Components;
+
+public class NavigationMenuViewComponent : ViewComponent
+{
+    public string Invoke()
+    {
+        return "Call nav view component";
+    }
+}
