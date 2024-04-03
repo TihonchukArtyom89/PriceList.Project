9efda0b0211092a73b8a6e536c8983ea93976fcb
TihonchukArtyom89 <mortus1989@gmail.com>
2024-04-03 17:34:59 +0300

    add class (NavigationMenuViewComponentTests cs in PriceList Tests project) to testing view components list for category of product on future testing, add view for navigation menu view component (Default cshtml in Views/Shared/Components/NavigationMenu folder), change todo list(TODO)

---
+++ PriceList.Tests/NavigationMenuViewComponentTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using Moq;
+using PriceList.WebApplication.Components;
+using PriceList.WebApplication.Models;
+using Xunit;
+
+namespace PriceList.Tests;
+
+public class NavigationMenuViewComponentTests
+{
+    //добавлять тесты для компонента представлений список категорий продуктов
+}
